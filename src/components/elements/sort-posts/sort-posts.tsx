34bb8da62c1ee52c149f829a4be84e8619ec2e25
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2024-11-28 03:19:33 +0100

    feat(sort-posts): add sorting functionality with callback for sort option changes

--- src/components/elements/sort-posts/sort-posts.tsx
+++ src/components/elements/sort-posts/sort-posts.tsx
@@ -2,25 +2,34 @@
 import { AnimatePresence } from "framer-motion";
 import { useRef, useState } from "react";
 
-import ViewDayTwoToneIcon from '@mui/icons-material/ViewDayTwoTone';
+import ViewDayTwoToneIcon from "@mui/icons-material/ViewDayTwoTone";
 import { Menu, MenuItem } from "@/components/elements/menu";
 import { Modal } from "@/components/elements/modal";
 
 import { Button } from "../button";
 import { Tooltip } from "../tooltip";
 
-export const SortPosts = () => {
+export const SortPosts = ({
+  onSortChange,
+}: {
+  onSortChange: (sortOption: string) => void;
+}) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const buttonRef = useRef<HTMLButtonElement>(null);
 
+  const handleSortChange = (sortOption: string) => {
+    onSortChange(sortOption);
+    setIsModalOpen(false);
+  };
+
   return (
     <div className="relative">
-      <Tooltip text="Sort Tweets">
+      <Tooltip text="Sort Posts">
         <Button
           ref={buttonRef}
           aria-expanded={isModalOpen}
           aria-haspopup="menu"
-          aria-label="Sort Tweets"
+          aria-label="Sort Posts"
           onClick={() => setIsModalOpen(true)}
           className="hover:bg-neutral-500 focus-visible:bg-neutral-500 focus-visible:outline-secondary-100 active:bg-neutral-600"
         >
@@ -32,9 +41,13 @@ export const SortPosts = () => {
         {isModalOpen && (
           <Modal onClose={() => setIsModalOpen(false)} background="none">
             <Menu onClose={() => setIsModalOpen(false)} ref={buttonRef}>
-              <MenuItem onClick={() => setIsModalOpen(false)}>Default</MenuItem>
-              <MenuItem onClick={() => setIsModalOpen(false)}>By date</MenuItem>
-              <MenuItem onClick={() => setIsModalOpen(false)}>
+              <MenuItem onClick={() => handleSortChange("default")}>
+                Default
+              </MenuItem>
+              <MenuItem onClick={() => handleSortChange("date")}>
+                By date
+              </MenuItem>
+              <MenuItem onClick={() => handleSortChange("popularity")}>
                 By popularity
               </MenuItem>
             </Menu>
