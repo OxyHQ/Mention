d5097aa174752140c6068be8a85ba60efc5cbfb3
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2025-01-12 23:09:57 +0100

    Refactor icon components to accept customizable color, size, and style props for improved flexibility

--- assets/icons/bookmark-icon.tsx
+++ assets/icons/bookmark-icon.tsx
@@ -1,6 +1,8 @@
-export const Bookmark = () => {
+import { colors } from '@/styles/colors'
+import { CSSProperties } from 'react';
+export const Bookmark = ({ color = colors.primaryColor, size = 26, style }: { color?: string; size?: number; style?: CSSProperties }) => {
   return (
-    <svg className="svgIcon" viewBox="0 0 24 24" aria-hidden="true">
+    <svg className="svgIcon" viewBox="0 0 24 24" aria-hidden="true" style={{ ...style, width: size, height: size, fill: color }}>
       <g>
         <path d="M4 4.5C4 3.12 5.119 2 6.5 2h11C18.881 2 20 3.12 20 4.5v18.44l-8-5.71-8 5.71V4.5zM6.5 4c-.276 0-.5.22-.5.5v14.56l6-4.29 6 4.29V4.5c0-.28-.224-.5-.5-.5h-11z"></path>
       </g>
@@ -8,9 +10,9 @@ export const Bookmark = () => {
   );
 };
 
-export const BookmarkActive = () => {
+export const BookmarkActive = ({ color = colors.primaryColor, size = 26, style }: { color?: string; size?: number; style?: CSSProperties }) => {
   return (
-    <svg className="svgIcon" viewBox="0 0 24 24" aria-hidden="true">
+    <svg className="svgIcon" viewBox="0 0 24 24" aria-hidden="true" style={{ ...style, width: size, height: size, fill: color }}>
       <g>
         <path d="M4 4.5C4 3.12 5.119 2 6.5 2h11C18.881 2 20 3.12 20 4.5v18.44l-8-5.71-8 5.71V4.5z"></path>
       </g>
--- assets/icons/hashtag-icon.tsx
+++ assets/icons/hashtag-icon.tsx
@@ -1,6 +1,7 @@
-export const Hashtag = () => {
+import { colors } from '@/styles/colors'
+export const Hashtag = ({ color = colors.primaryColor, size = 26, style }: { color?: string; size?: number; style?: CSSProperties }) => {
   return (
-    <svg className="svgIcon" viewBox="0 0 24 24" aria-hidden="true">
+    <svg className="svgIcon" viewBox="0 0 24 24" aria-hidden="true" style={{ ...style, width: size, height: size, fill: color }}>
       <g>
         <path d="M10.09 3.098L9.72 7h5.99l.39-4.089 1.99.187L17.72 7h3.78v2h-3.97l-.56 6h3.53v2h-3.72l-.38 4.089-1.99-.187.36-3.902H8.78l-.38 4.089-1.99-.187L6.77 17H2.5v-2h4.46l.56-6H3.5V7h4.21l.39-4.089 1.99.187zM14.96 15l.56-6H9.53l-.56 6h5.99z"></path>
       </g>
@@ -8,9 +9,9 @@ export const Hashtag = () => {
   );
 };
 
-export const HashtagActive = () => {
+export const HashtagActive = ({ color = colors.primaryColor, size = 26, style }: { color?: string; size?: number; style?: CSSProperties }) => {
   return (
-    <svg className="svgIcon" viewBox="0 0 24 24" aria-hidden="true">
+    <svg className="svgIcon" viewBox="0 0 24 24" aria-hidden="true" style={{ ...style, width: size, height: size, fill: color }}>
       <g>
         <path d="M10.64 3.157l-.36 3.593h4.99l.38-3.892 2.99.299-.36 3.593h2.97v2.5h-3.22l-.55 5.5h2.77v2.5h-3.02l-.39 3.892-2.98-.299.36-3.593H9.23l-.39 3.892-2.98-.299.36-3.593H2.75v-2.5h3.72l.55-5.5H3.75v-2.5h3.52l.38-3.892 2.99.299zm3.83 11.593l.55-5.5h-4.99l-.55 5.5h4.99z"></path>
       </g>
