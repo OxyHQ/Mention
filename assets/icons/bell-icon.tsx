39e5d0efceaf4d81ea649c4a7bf88285262ca2bd
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2025-01-14 20:40:09 +0100

    Refactor icon components to use react-native-svg for improved rendering and performance

--- assets/icons/bell-icon.tsx
+++ assets/icons/bell-icon.tsx
@@ -1,21 +1,21 @@
-import { colors } from '@/styles/colors'
-import { CSSProperties } from 'react';
-export const BellActive = ({ color = colors.primaryColor, size = 26, style }: { color?: string; size?: number; style?: CSSProperties }) => {
+import React from 'react';
+import Svg, { Path, Line, Polygon } from 'react-native-svg';
+import { ViewStyle } from 'react-native';
+import { colors } from '@/styles/colors';
+
+
+export const BellActive = ({ color = colors.primaryColor, size = 26, style }: { color?: string; size?: number; style?: ViewStyle }) => {
   return (
-    <svg className="svgIcon" viewBox="0 0 24 24" aria-hidden="true" style={{ ...style, width: size, height: size, fill: color }}>
-      <g>
-        <path d="M11.996 2c-4.062 0-7.49 3.021-7.999 7.051L2.866 18H7.1c.463 2.282 2.481 4 4.9 4s4.437-1.718 4.9-4h4.236l-1.143-8.958C19.48 5.017 16.054 2 11.996 2zM9.171 18h5.658c-.412 1.165-1.523 2-2.829 2s-2.417-.835-2.829-2z"></path>
-      </g>
-    </svg>
+    <Svg viewBox="0 0 24 24" width={size} height={size} style={{ ...style }}>
+      <Path fill={color} d="M11.996 2c-4.062 0-7.49 3.021-7.999 7.051L2.866 18H7.1c.463 2.282 2.481 4 4.9 4s4.437-1.718 4.9-4h4.236l-1.143-8.958C19.48 5.017 16.054 2 11.996 2zM9.171 18h5.658c-.412 1.165-1.523 2-2.829 2s-2.417-.835-2.829-2z"></Path>
+    </Svg>
   );
 };
 
-export const Bell = ({ color = colors.primaryColor, size = 26, style }: { color?: string; size?: number; style?: CSSProperties }) => {
+export const Bell = ({ color = colors.primaryColor, size = 26, style }: { color?: string; size?: number; style?: ViewStyle }) => {
   return (
-    <svg className="svgIcon" viewBox="0 0 24 24" aria-hidden="true" style={{ ...style, width: size, height: size, fill: color }}>
-      <g>
-        <path d="M19.993 9.042C19.48 5.017 16.054 2 11.996 2s-7.49 3.021-7.999 7.051L2.866 18H7.1c.463 2.282 2.481 4 4.9 4s4.437-1.718 4.9-4h4.236l-1.143-8.958zM12 20c-1.306 0-2.417-.835-2.829-2h5.658c-.412 1.165-1.523 2-2.829 2zm-6.866-4l.847-6.698C6.364 6.272 8.941 4 11.996 4s5.627 2.268 6.013 5.295L18.864 16H5.134z"></path>
-      </g>
-    </svg>
+    <Svg viewBox="0 0 24 24" width={size} height={size} style={{ ...style }}>
+      <Path fill={color} d="M19.993 9.042C19.48 5.017 16.054 2 11.996 2s-7.49 3.021-7.999 7.051L2.866 18H7.1c.463 2.282 2.481 4 4.9 4s4.437-1.718 4.9-4h4.236l-1.143-8.958zM12 20c-1.306 0-2.417-.835-2.829-2h5.658c-.412 1.165-1.523 2-2.829 2zm-6.866-4l.847-6.698C6.364 6.272 8.941 4 11.996 4s5.627 2.268 6.013 5.295L18.864 16H5.134z"></Path>
+    </Svg>
   );
 };
--- assets/mention-logo.tsx
+++ assets/mention-logo.tsx
@@ -1,14 +1,17 @@
-import React, { CSSProperties } from 'react';
-import { colors } from '@/styles/colors'
+import React from 'react';
+import Svg, { Path, Line, Polygon } from 'react-native-svg';
+import { ViewStyle } from 'react-native';
+import { colors } from '@/styles/colors';
 
-export const MentionLogo = ({ color = colors.primaryColor, size = 48, style }: { color?: string; size?: number; style?: CSSProperties }) => {
+
+export const MentionLogo = ({ color = colors.primaryColor, size = 26, style }: { color?: string; size?: number; style?: ViewStyle }) => {
   return (
-    <svg viewBox="0 0 196 196" aria-label="Mention" role="img" style={{ ...style, width: size, height: size }}>
+    <Svg viewBox="0 0 196 196" width={size} height={size} style={{ ...style, transform: [{ translateY: 2 }] }}>
       <path
         xmlns="http://www.w3.org/2000/svg"
         d="M 131.991 97.92 L 131.991 194.648 L 163.437 194.648 L 194.883 194.648 L 194.883 97.92 L 194.883 1.192 L 163.437 1.192 L 131.991 1.192 L 131.991 97.92 M 62.299 97.071 L 62.299 174.284 L 87.795 174.284 L 113.294 174.284 L 113.294 97.071 L 113.294 19.86 L 87.795 19.86 L 62.299 19.86 L 62.299 97.071 M 1.108 97.92 L 1.108 153.921 L 22.355 153.921 L 43.603 153.921 L 43.603 97.92 L 43.603 41.92 L 22.355 41.92 L 1.108 41.92"
         fill={color}
       />
-    </svg>
+    </Svg>
   );
 };
