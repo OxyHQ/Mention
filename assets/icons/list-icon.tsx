2206aeef3fb6b9673ea5181f6de0e1f3993d6ee9
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2025-01-14 20:55:27 +0100

    Refactor icon components to use react-native-svg for improved compatibility and performance

--- assets/icons/list-icon.tsx
+++ assets/icons/list-icon.tsx
@@ -1,17 +1,21 @@
-import { colors } from '@/styles/colors'
-import { CSSProperties } from 'react';
-export const List = ({ color = colors.primaryColor, size = 26, style }: { color?: string; size?: number; style?: CSSProperties }) => {
+import React from 'react';
+import Svg, { Path, Line, Polygon } from 'react-native-svg';
+import { ViewStyle } from 'react-native';
+import { colors } from '@/styles/colors';
+
+
+export const List = ({ color = colors.primaryColor, size = 26, style }: { color?: string; size?: number; style?: ViewStyle }) => {
   return (
-    <svg fill="none" width={size} viewBox="0 0 24 24" height={size} aria-hidden="true" style={{ ...style, color }}>
-      <path fill={color} fill-rule="evenodd" clip-rule="evenodd" d="M6 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2ZM3 7a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm9 0a1 1 0 0 1 1-1h7a1 1 0 1 1 0 2h-7a1 1 0 0 1-1-1Zm-6 9a1 1 0 1 0 0 2 1 1 0 0 0 0-2Zm-3 1a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm9 0a1 1 0 0 1 1-1h7a1 1 0 1 1 0 2h-7a1 1 0 0 1-1-1Z"></path>
-    </svg>
+    <Svg viewBox="0 0 24 24" width={size} height={size} style={{ ...style }}>
+      <Path fill={color} fill-rule="evenodd" clip-rule="evenodd" d="M6 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2ZM3 7a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm9 0a1 1 0 0 1 1-1h7a1 1 0 1 1 0 2h-7a1 1 0 0 1-1-1Zm-6 9a1 1 0 1 0 0 2 1 1 0 0 0 0-2Zm-3 1a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm9 0a1 1 0 0 1 1-1h7a1 1 0 1 1 0 2h-7a1 1 0 0 1-1-1Z"></Path>
+    </Svg>
   );
 };
 
-export const ListActive = ({ color = colors.primaryColor, size = 26, style }: { color?: string; size?: number; style?: CSSProperties }) => {
+export const ListActive = ({ color = colors.primaryColor, size = 26, style }: { color?: string; size?: number; style?: ViewStyle }) => {
   return (
-    <svg fill="none" width={size} viewBox="0 0 24 24" height={size} aria-hidden="true" style={{ ...style, color }}>
-      <path fill={color} fill-rule="evenodd" clip-rule="evenodd" d="M3 7a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm0 10a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm10-1a1 1 0 1 0 0 2h7a1 1 0 1 0 0-2h-7Zm-1-9a1 1 0 0 1 1-1h7a1 1 0 1 1 0 2h-7a1 1 0 0 1-1-1Z"></path>
-    </svg>
+    <Svg viewBox="0 0 24 24" width={size} height={size} style={{ ...style }}>
+      <Path fill={color} fill-rule="evenodd" clip-rule="evenodd" d="M3 7a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm0 10a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm10-1a1 1 0 1 0 0 2h7a1 1 0 1 0 0-2h-7Zm-1-9a1 1 0 0 1 1-1h7a1 1 0 1 1 0 2h-7a1 1 0 0 1-1-1Z"></Path>
+    </Svg>
   );
 };
--- assets/icons/plus-icon.tsx
+++ assets/icons/plus-icon.tsx
@@ -1,11 +1,13 @@
-import { colors } from '@/styles/colors'
-import { CSSProperties } from 'react';
-export const Plus = ({ color = colors.primaryColor, size = 26, style }: { color?: string; size?: number; style?: CSSProperties }) => {
+import React from 'react';
+import Svg, { Path, Line, Polygon } from 'react-native-svg';
+import { ViewStyle } from 'react-native';
+import { colors } from '@/styles/colors';
+
+
+export const Plus = ({ color = colors.primaryColor, size = 26, style }: { color?: string; size?: number; style?: ViewStyle }) => {
   return (
-    <svg className="svgIcon" viewBox="0 0 24 24" aria-hidden="true" style={{ ...style, width: size, height: size, fill: color }}>
-      <g>
-        <path d="M11 11V4h2v7h7v2h-7v7h-2v-7H4v-2h7z"></path>
-      </g>
-    </svg>
+    <Svg viewBox="0 0 24 24" width={size} height={size} style={{ ...style }}>
+      <Path d="M11 11V4h2v7h7v2h-7v7h-2v-7H4v-2h7z"></Path>
+    </Svg>
   );
-};
+}; 
