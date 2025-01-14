abc561caa063c77b234ba1de5b010d143d983ff7
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2025-01-14 14:52:37 +0100

    Add LocationIcon component with customizable color, size, and style props

--- assets/icons/location-icon.tsx
+++ assets/icons/location-icon.tsx
@@ -1,4 +1,9 @@
-import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
-export const LocationIcon = () => {
-  return <LocationOnOutlinedIcon />;
+import { colors } from '@/styles/colors'
+import { CSSProperties } from 'react';
+export const LocationIcon = ({ color = colors.primaryColor, size = 26, style }: { color?: string; size?: number; style?: CSSProperties }) => {
+  return (
+    <svg fill="currentColor" width={size} viewBox="0 0 24 24" height={size} aria-hidden="true" style={{ ...style, color }}>
+      <path d="M12.053 8.105a1.604 1.604 0 1 0 1.604 1.604 1.604 1.604 0 0 0-1.604-1.604Zm0-7.105a8.684 8.684 0 0 0-8.708 8.66c0 5.699 6.14 11.495 8.108 13.123a.939.939 0 0 0 1.2 0c1.969-1.628 8.109-7.424 8.109-13.123A8.684 8.684 0 0 0 12.053 1Zm0 19.662C9.29 18.198 5.345 13.645 5.345 9.66a6.709 6.709 0 0 1 13.417 0c0 3.985-3.944 8.538-6.709 11.002Z"></path>
+    </svg>
+  );
 };
