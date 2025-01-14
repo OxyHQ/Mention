a5e2563822bfed3794708a5f9a9e47059535f89f
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2025-01-14 20:37:17 +0100

    Refactor icon components to remove unnecessary transform styles for improved layout consistency

--- assets/icons/bookmark-icon.tsx
+++ assets/icons/bookmark-icon.tsx
@@ -6,7 +6,7 @@ import { colors } from '@/styles/colors';
 
 export const Bookmark = ({ color = colors.primaryColor, size = 26, style }: { color?: string; size?: number; style?: ViewStyle }) => {
   return (
-    <Svg viewBox="0 0 24 24" width={size} height={size} style={{ ...style, transform: [{ translateY: 2 }] }}>
+    <Svg viewBox="0 0 24 24" width={size} height={size} style={{ ...style }}>
       <Path d="M4 4.5C4 3.12 5.119 2 6.5 2h11C18.881 2 20 3.12 20 4.5v18.44l-8-5.71-8 5.71V4.5zM6.5 4c-.276 0-.5.22-.5.5v14.56l6-4.29 6 4.29V4.5c0-.28-.224-.5-.5-.5h-11z"></Path>
     </Svg>
   );
@@ -14,7 +14,7 @@ export const Bookmark = ({ color = colors.primaryColor, size = 26, style }: { co
 
 export const BookmarkActive = ({ color = colors.primaryColor, size = 26, style }: { color?: string; size?: number; style?: ViewStyle }) => {
   return (
-    <Svg viewBox="0 0 24 24" width={size} height={size} style={{ ...style, transform: [{ translateY: 2 }] }}>
+    <Svg viewBox="0 0 24 24" width={size} height={size} style={{ ...style }}>
       <Path d="M4 4.5C4 3.12 5.119 2 6.5 2h11C18.881 2 20 3.12 20 4.5v18.44l-8-5.71-8 5.71V4.5z"></Path>
     </Svg>
   );
