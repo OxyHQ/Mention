a3992fdc05c71fdc5d9f248e550043805f749c32
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2025-01-24 03:56:43 +0100

    Refactor SettingsAboutScreen to enhance UI with setting items; implement reusable SettingItem component; update styles and integrate translation support

--- app/settings/about.tsx
+++ app/settings/about.tsx
@@ -1,38 +1,133 @@
-import React, { useRef } from 'react';
-import { View, Pressable, Text } from 'react-native';
-import InstagramStories, { InstagramStoriesPublicMethods } from '@birdwingo/react-native-instagram-stories';
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TextInput, ScrollView, SafeAreaView, TouchableOpacity, FlatList } from "react-native";
+import { Post as PostType } from "@/interfaces/Post";
+import { useTranslation } from "react-i18next";
+import { Ionicons } from "@expo/vector-icons";
+import { Stack, Link } from "expo-router";
+import { colors } from "@/styles/colors";
+import { Header } from "@/components/Header";
+import { toast } from '@/lib/sonner';
+import Avatar from "@/components/Avatar";
 
-const YourComponent = () => {
+interface SettingItemProps {
+    icon: string;
+    title: string;
+    subtitle?: string;
+    link?: string;
+    onPress?: () => void;
+    content?: string;
+}
 
-    // to use public methods:
-    const ref = useRef<InstagramStoriesPublicMethods>(null);
+const SettingItem: React.FC<SettingItemProps> = ({ icon, title, subtitle, link, onPress, content }) => (
+    <Link href={link as any} asChild>
+        <TouchableOpacity style={styles.settingItem} onPress={onPress}>
+            <View style={styles.iconContainer}>
+                <Ionicons name={icon as any} size={24} color="#333" />
+            </View>
+            <View style={styles.textContainer}>
+                <Text style={styles.title}>{title}</Text>
+                {subtitle ? <Text style={styles.subtitle}>{subtitle}</Text> : null}
+            </View>
+            <Text style={styles.content}>{content}</Text>
+        </TouchableOpacity>
+    </Link>
+);
 
-    const stories = [{
-        id: 'user1',
-        name: 'User 1',
-        avatarSource: { uri: 'https://upload.wikimedia.org/wikipedia/commons/3/32/Googleplex_HQ_%28cropped%29.jpg' },
-        stories: [
-            { id: 'story1', source: { uri: 'https://upload.wikimedia.org/wikipedia/commons/3/32/Googleplex_HQ_%28cropped%29.jpg' } },
-            // ...
-        ]
-    }, // ...
-    ];
-
-    // usage of public method
-    const setStories = () => ref.current?.setStories(stories);
+export default function SettingsAboutScreen() {
+    const { t } = useTranslation();
 
     return (
-        <View>
-            <InstagramStories
-                ref={ref}
-                stories={stories}
-            // ...
-            />
-            <Pressable onPress={setStories}>
-                <Text>Set Stories</Text>
-            </Pressable>
-        </View>
+        <>
+            <Stack.Screen options={{ title: t("Settings") }} />
+            <SafeAreaView style={styles.container}>
+                <Header options={{
+                    leftComponents: [<Ionicons name="settings" size={24} color={colors.COLOR_BLACK} />],
+                    title: t("Settings"),
+                    rightComponents: [<Ionicons name="add" size={24} color={colors.COLOR_BLACK} onPress={() => toast('My first toast')} />],
+                }} />
+                <SettingItem
+                    icon="document-text"
+                    title={t('Terms of Service')}
+                    link="/"
+                />
+                <SettingItem
+                    icon="shield-checkmark"
+                    title={t('Privacy Policy')}
+                    link="/"
+                />
+                <SettingItem
+                    icon="bug"
+                    title={t('System log')}
+                    link="/"
+                />
+                <SettingItem
+                    icon="information-circle"
+                    title={t('Version')}
+                    subtitle={t('1.0.0')}
+                    link="/"
+                    content="1.0.0"
+                />
+            </SafeAreaView>
+        </>
     );
-};
+}
 
-export default YourComponent;
+const styles = StyleSheet.create({
+    container: {
+    },
+    searchContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        margin: 16,
+        padding: 8,
+        backgroundColor: colors.primaryLight,
+        borderWidth: 1,
+        borderColor: colors.COLOR_BLACK_LIGHT_6,
+        borderRadius: 35,
+    },
+    searchIcon: {
+        marginHorizontal: 8,
+    },
+    searchInput: {
+        flex: 1,
+        fontSize: 16,
+        color: '#000',
+        paddingVertical: 8,
+    },
+    scrollView: {
+        flex: 1,
+    },
+    settingItem: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 10,
+        backgroundColor: '#fff',
+        borderBottomWidth: 1,
+        borderColor: colors.COLOR_BLACK_LIGHT_6,
+    },
+    iconContainer: {
+        width: 40,
+        height: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: 10,
+    },
+    textContainer: {
+        flex: 1,
+    },
+    title: {
+        fontSize: 16,
+        fontWeight: '500',
+        color: '#000',
+        marginBottom: 4,
+    },
+    subtitle: {
+        fontSize: 14,
+        color: '#666',
+    },
+    content: {
+        fontSize: 16,
+        color: '#666',
+        paddingEnd: 15,
+    }
+});
