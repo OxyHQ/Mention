a012d4286d994c7bd1f194237a7f9cd0a696352d
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2025-01-31 18:09:03 +0100

    Refactor Post interface; remove unused fields to streamline data structure

--- interfaces/Post.ts
+++ interfaces/Post.ts
@@ -13,14 +13,6 @@ export interface Post {
   id: string;
   text: string;
   source: string;
-  in_reply_to_user_id: string | null;
-  in_reply_to_username: string | null;
-  is_quote_status: boolean;
-  quoted_status_id: string | null;
-  quote_count: number;
-  reply_count: number;
-  repost_count: number;
-  favorite_count: number;
   possibly_sensitive: boolean;
   lang: string;
   created_at: string;
@@ -29,14 +21,8 @@ export interface Post {
   in_reply_to_status_id: string | null;
   author_id: string;
   author: Author;
-  likes: number;
-  replies: number;
   media: any[];
-  reposts: number;
   quoted_post: any | null;
-  quotes: number;
-  comments: number;
-  bookmarks: number;
   _count: {
     comments: number;
     likes: number;
