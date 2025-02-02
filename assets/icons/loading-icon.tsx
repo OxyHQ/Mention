b3270fe28f70919bb3c9915ef1b125bbc9a4b3ca
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2025-02-02 13:30:09 +0100

    Add Loading component with animated SVG icon

---
+++ assets/icons/loading-icon.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import Svg, { Rect } from 'react-native-svg';
+import { ViewStyle } from 'react-native';
+import { colors } from '@/styles/colors';
+import { Animated, Easing } from 'react-native';
+
+export const Loading = ({ color = colors.primaryColor, size = 26, style }: { color?: string; size?: number; style?: ViewStyle }) => {
+    const rotateAnim = React.useRef(new Animated.Value(0)).current;
+
+    React.useEffect(() => {
+        const animation = Animated.loop(
+            Animated.timing(rotateAnim, {
+                toValue: 1,
+                duration: 1000,
+                easing: Easing.linear,
+                useNativeDriver: false,
+            })
+        );
+        rotateAnim.setValue(0); // Reset the animation value
+        animation.start();
+        return () => animation.stop();
+    }, [rotateAnim]);
+
+    const rotate = rotateAnim.interpolate({
+        inputRange: [0, 1],
+        outputRange: ['0deg', '360deg'],
+    });
+
+    return (
+        <Animated.View style={{ transform: [{ rotate }], width: size, height: size, alignItems: 'center', margin: 'auto', justifyContent: 'center', ...style }}>
+            <Svg viewBox="0 0 100 100" width={size} height={size}>
+                <Rect fill={color} height="10" opacity="0" rx="5" ry="5" transform="rotate(-90 50 50)" width="28" x="67" y="45"></Rect>
+                <Rect fill={color} height="10" opacity="0.125" rx="5" ry="5" transform="rotate(-45 50 50)" width="28" x="67" y="45"></Rect>
+                <Rect fill={color} height="10" opacity="0.25" rx="5" ry="5" transform="rotate(0 50 50)" width="28" x="67" y="45"></Rect>
+                <Rect fill={color} height="10" opacity="0.375" rx="5" ry="5" transform="rotate(45 50 50)" width="28" x="67" y="45"></Rect>
+                <Rect fill={color} height="10" opacity="0.5" rx="5" ry="5" transform="rotate(90 50 50)" width="28" x="67" y="45"></Rect>
+                <Rect fill={color} height="10" opacity="0.625" rx="5" ry="5" transform="rotate(135 50 50)" width="28" x="67" y="45"></Rect>
+                <Rect fill={color} height="10" opacity="0.75" rx="5" ry="5" transform="rotate(180 50 50)" width="28" x="67" y="45"></Rect>
+                <Rect fill={color} height="10" opacity="0.875" rx="5" ry="5" transform="rotate(225 50 50)" width="28" x="67" y="45"></Rect>
+            </Svg>
+        </Animated.View>
+    );
+};
