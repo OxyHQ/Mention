10ea6e6aed4960d800fa127796d71472e0f20ffe
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2025-02-02 09:04:49 +0100

    Remove TrendComponent from RightBar for code cleanup

--- components/RightBar.tsx
+++ components/RightBar.tsx
@@ -156,60 +156,6 @@ const FollowRowComponent = ({ profileData }: { profileData: ProfileData }) => {
     )
 }
 
-const TrendComponent = ({
-    topHeader,
-    mainTitle,
-    numberOfPosts,
-}: {
-    topHeader: string
-    mainTitle: string
-    numberOfPosts: string
-}) => {
-    return (
-        <Link href={mainTitle as any} style={styles.trendItem}>
-            <View
-                style={{
-                    flex: 1,
-                    justifyContent: 'space-between',
-                }}>
-                <Text style={{ fontSize: 13, color: colors.COLOR_BLACK_LIGHT_4 }}>
-                    {topHeader}
-                </Text>
-                <Text style={{ fontSize: 15, fontWeight: 'bold', paddingVertical: 3 }}>
-                    {mainTitle}
-                </Text>
-                <Text style={{ fontSize: 14, color: colors.COLOR_BLACK_LIGHT_4 }}>
-                    {numberOfPosts}
-                </Text>
-            </View>
-            <Pressable
-                style={({ hovered }) => [
-                    hovered
-                        ? {
-                            backgroundColor: colors.COLOR_BLACK_LIGHT_6,
-                        }
-                        : {},
-                    {
-                        borderRadius: 100,
-                        width: 40,
-                        height: 40,
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        marginTop: -20,
-                    },
-                ]}>
-                <Ionicons
-                    name="ellipsis-horizontal"
-                    style={{
-                        fontSize: 20,
-                        color: colors.COLOR_BLACK_LIGHT_5,
-                    }}
-                />
-            </Pressable>
-        </Link>
-    )
-}
-
 const styles = StyleSheet.create({
     container: {
         width: 350,
