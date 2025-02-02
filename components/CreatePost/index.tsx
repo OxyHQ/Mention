f7e8ec89cb51dfa8d3ff76194ee1c816f3a2d777
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2025-02-02 06:29:53 +0100

    Refactor CreatePost component to use Avatar component and update post author ID field

--- components/CreatePost/index.tsx
+++ components/CreatePost/index.tsx
@@ -22,6 +22,7 @@ import EmojiPicker from 'emoji-picker-react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchPosts, createPost } from '@/store/reducers/postsReducer';
 import FileSelectorModal from '@/modules/oxyhqservices/components/FileSelectorModal';
+import Avatar from '../Avatar';
 
 interface Props {
     style?: ViewStyle
@@ -46,7 +47,7 @@ export const CreatePost: React.FC<Props> = ({ style, onClose }) => {
     const post = () => {
         if (data) {
             const newPost = {
-                author_id: '678b29d19085a13337ca9fd4',
+                userID: '678b29d19085a13337ca9fd4',
                 text: data,
                 media: selectedMedia.map(media => media.id),
             };
@@ -94,11 +95,9 @@ export const CreatePost: React.FC<Props> = ({ style, onClose }) => {
                 </Pressable>
             </View>
             <View style={styles.middleRow}>
-                <Image
+                <Avatar
                     style={styles.profileImage}
-                    source={{
-                        uri: 'https://scontent-bcn1-1.xx.fbcdn.net/v/t39.30808-6/463417298_3945442859019280_8807009322776007473_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=zXRqATKNOw0Q7kNvgHnyfUU&_nc_oc=AdgYVSd5vfuRV96_nxCmCnemTuCfkgS2YQ_Diu1puFc_h76AbObPG9_eD5rFA5TcRxYnE2mW_ZfJKWuXYtX-Z8ue&_nc_zt=23&_nc_ht=scontent-bcn1-1.xx&_nc_gid=AqvR1nQbgt2nJudR3eAKaLM&oh=00_AYBD3grUDwAE84jgvGS3UmB93xn3odRDqePjARpVj6L2vQ&oe=678C0857',
-                    }}
+                    id=""
                 />
                 <TextInput
                     style={styles.middleRowText}
