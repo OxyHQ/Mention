5d19230c1e7f0b822de9fc04a1fd24a82ea2a506
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2025-02-04 03:27:18 +0100

    Add dotenv and module resolver plugins to Babel configuration

--- babel.config.js
+++ babel.config.js
@@ -3,8 +3,16 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-          '@babel/plugin-proposal-export-namespace-from',
-          'react-native-reanimated/plugin',
+        ['module:react-native-dotenv'],
+        ['module-resolver', {
+          root: ['./src'],
+          alias: {
+            '@': './src',
+          }
+        }],
+        'module:metro-react-native-babel-preset',
+        '@babel/plugin-proposal-export-namespace-from',
+        'react-native-reanimated/plugin',
       ],
   };
 };
