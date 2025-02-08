d4688e3b98a91beb30e5985eb4e98252694f086c
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2025-02-08 04:53:31 +0100

    Refactor Profile interface to enhance user data structure and add additional fields

--- interfaces/Profile.ts
+++ interfaces/Profile.ts
@@ -1,12 +1,34 @@
 export interface Profile {
   id: string;
-  name: string;
-  avatar: string;
-  username: string;
+  userID: string;
+  name: {
+    first?: string;
+    last?: string;
+  };
+  avatar?: string;
+  username?: string;
   bio?: string;
   location?: string;
   website?: string;
   joinDate?: string;
+  description?: string;
+  banner?: string;
+  associated?: {
+    lists: number;
+    feedgens: number;
+    starterPacks: number;
+    labeler: boolean;
+  };
+  labels?: string[];
+  created_at?: string;
+  indexedAt?: string;
+  followersCount?: number;
+  followsCount?: number;
+  postsCount?: number;
+  pinnedPost?: {
+    cid: string;
+    uri: string;
+  };
   _count?: {
     followers: number;
     following: number;
