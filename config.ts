1baf1913562296450c262518f81ea46e971dabeb
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2025-02-09 19:28:50 +0100

    Fix OXY_CLOUD_URL to ensure proper URL formatting with trailing slash

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 export const API_URL = process.env.API_URL || "http://localhost:3000/api";
 export const API_URL_OXY = process.env.API_URL_OXY || "http://localhost:3000/api";
 export const API_URL_SOCKET = process.env.API_URL_SOCKET || "http://localhost:3000";
-export const OXY_CLOUD_URL = process.env.OXY_CLOUD_URL || "http://localhost:3000/api/files";
+export const OXY_CLOUD_URL = process.env.OXY_CLOUD_URL || "http://localhost:3000/api/files/";
 export const API_URL_SOCKET_CHAT = process.env.API_URL_SOCKET_CHAT || "http://localhost:3000";
 export const API_OXY_CHAT = process.env.API_OXY_CHAT || "http://localhost:3000/api";
