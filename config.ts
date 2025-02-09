5817e4d9c4ce5bb9041baeea7e726463bbafc3f2
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2025-02-09 07:55:23 +0100

    Add configuration file for API and socket URLs

---
+++ config.ts
@@ -0,0 +1,5 @@
+export const API_URL = process.env.API_URL || "http://192.168.1.196:3000/api";
+export const API_URL_OXY = process.env.API_URL_OXY || "http://192.168.1.196:3000/api";
+export const API_URL_SOCKET = process.env.API_URL_SOCKET || "http://192.168.1.196:3000/api";
+export const OXY_CLOUD_URL = process.env.OXY_CLOUD_URL || "http://192.168.1.196:3000/api/files/";
+export const API_URL_SOCKET_CHAT = process.env.API_URL_SOCKET_CHAT || "http://192.168.1.196:3000/api";
