d8e03fc317ed107929e73bff2700fff70ded7777
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2025-02-09 20:50:52 +0100

    Refactor WebSplashScreen component to use Tailwind CSS classes and remove unused styles

--- components/WebSplashScreen.tsx
+++ components/WebSplashScreen.tsx
@@ -1,32 +1,14 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View } from 'react-native';
+import { MentionLogo } from '@/assets/mention-logo';
 import { colors } from '@/styles/colors';
-import { MentionLogo } from '@/assets/mention-logo'; // Adjust the path to your logo image
 
 const WebSplashScreen = () => {
     return (
-        <View style={styles.container}>
+        <View className="flex-1 items-center justify-center bg-primary-light dark:bg-primary-dark">
             <MentionLogo size={80} color={colors.primaryColor} />
         </View>
     );
 };
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: colors.COLOR_BACKGROUND,
-    },
-    logo: {
-        width: 100,
-        height: 100,
-        marginBottom: 20,
-    },
-    text: {
-        fontSize: 24,
-        color: colors.primaryColor,
-    },
-});
 
 export default WebSplashScreen;
