d347732a8c454e589d6df3adb0e75be3a01c61b1
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2025-02-09 19:49:29 +0100

    Add expo-linear-gradient package and update tailwind config for component paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     // NOTE: Update this to include the paths to all of your component files.
-    content: ["./app/**/*.{js,jsx,ts,tsx}"],
+    content: ['./app/**/*.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
     presets: [require("nativewind/preset")],
     theme: {
       extend: {},
