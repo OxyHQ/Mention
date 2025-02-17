260690285bbfa123558d7fb917953e8765b84ba6
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2025-02-17 05:42:00 +0100

    Update Stories component to use OXY_CLOUD_URL for avatar images instead of API_URL

--- components/Stories.tsx
+++ components/Stories.tsx
@@ -1,7 +1,7 @@
 import React, { useRef } from 'react';
 import { View, Pressable, Text } from 'react-native';
 import InstagramStories, { InstagramStoriesPublicMethods } from '@birdwingo/react-native-instagram-stories';
-import { API_URL } from "@/config";
+import { API_URL, OXY_CLOUD_URL } from "@/config";
 
 export function Stories() {
 
@@ -11,7 +11,7 @@ export function Stories() {
     const stories = [{
         id: 'user1',
         name: 'Nate',
-        avatarSource: { uri: `${API_URL}/files/6790749544634262da8394f2` },
+        avatarSource: { uri: `${OXY_CLOUD_URL}/6790749544634262da8394f2` },
         stories: [
             { id: 'story1', source: { uri: 'https://upload.wikimedia.org/wikipedia/commons/3/32/Googleplex_HQ_%28cropped%29.jpg' } },
             // ...
