e34dcacfeaec5105e58b9b39b0c603b4ba62263f
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2025-02-26 11:01:15 +0100

    Update API URLs to use production endpoints for consistency

--- config.ts
+++ config.ts
@@ -6,8 +6,8 @@ export const SOCKET_URL = process.env.EXPO_PUBLIC_SOCKET_URL || 'http://localhos
 export const DEFAULT_AVATAR = 'https://mention.earth/default-avatar.png';
 export const DEFAULT_COVER = 'https://mention.earth/default-cover.png';
 
-export const API_URL_OXY = process.env.API_URL_OXY || "http://192.168.1.196:3000";
-export const API_URL_SOCKET = process.env.API_URL_SOCKET || "ws://192.168.1.196:3000"; // Remove  suffix
-export const OXY_CLOUD_URL = process.env.OXY_CLOUD_URL || "http://192.168.1.196:3000/files/";
-export const API_URL_SOCKET_CHAT = process.env.API_URL_SOCKET_CHAT || "http://192.168.1.196:3000";
-export const API_OXY_CHAT = process.env.API_OXY_CHAT || "http://192.168.1.196:3000";
+export const API_URL_OXY = process.env.API_URL_OXY || "http://api.mention.earth";
+export const API_URL_SOCKET = process.env.API_URL_SOCKET || "ws://api.mention.earth"; // Remove  suffix
+export const OXY_CLOUD_URL = process.env.OXY_CLOUD_URL || "http://api.mention.earth/files/";
+export const API_URL_SOCKET_CHAT = process.env.API_URL_SOCKET_CHAT || "http://api.mention.earth";
+export const API_OXY_CHAT = process.env.API_OXY_CHAT || "http://api.mention.earth";
