492708bf5405b03b7b73805cd2d25bef03f29cd1
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2025-02-28 15:28:00 +0100

    Update API and socket URLs to use secure HTTPS endpoints

--- config.ts
+++ config.ts
@@ -1,9 +1,9 @@
 // Base URLs
-export const API_URL = process.env.EXPO_PUBLIC_API_URL || 'http://localhost:3000';
-export const SOCKET_URL = process.env.EXPO_PUBLIC_SOCKET_URL || 'http://localhost:3000';
+export const API_URL = process.env.EXPO_PUBLIC_API_URL || 'https://api.mention.earth';
+export const SOCKET_URL = process.env.EXPO_PUBLIC_SOCKET_URL || 'https://api.mention.earth';
 
-export const API_URL_OXY = process.env.API_URL_OXY || "http://localhost:3000";
-export const API_URL_SOCKET = process.env.API_URL_SOCKET || "ws://localhost:3000"; // Remove  suffix
-export const OXY_CLOUD_URL = process.env.OXY_CLOUD_URL || "http://localhost:3000/files/";
-export const API_URL_SOCKET_CHAT = process.env.API_URL_SOCKET_CHAT || "http://localhost:3000";
-export const API_OXY_CHAT = process.env.API_OXY_CHAT || "http://localhost:3000";
+export const API_URL_OXY = process.env.API_URL_OXY || "https://api.mention.earth";
+export const API_URL_SOCKET = process.env.API_URL_SOCKET || "ws://api.mention.earth"; // Remove  suffix
+export const OXY_CLOUD_URL = process.env.OXY_CLOUD_URL || "https://api.mention.earth/files/";
+export const API_URL_SOCKET_CHAT = process.env.API_URL_SOCKET_CHAT || "https://api.mention.earth";
+export const API_OXY_CHAT = process.env.API_OXY_CHAT || "https://api.mention.earth";
