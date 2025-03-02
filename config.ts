960ea8e42e037f6507bdec16d2150370837fe66a
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2025-03-02 18:25:54 +0100

    Refactor API and socket URL configuration to use environment variables

--- config.ts
+++ config.ts
@@ -1,9 +1,7 @@
 // Base URLs
-export const API_URL = process.env.EXPO_PUBLIC_API_URL || 'https://api.mention.earth';
-export const SOCKET_URL = process.env.EXPO_PUBLIC_SOCKET_URL || 'https://api.mention.earth';
+export const API_URL = process.env.API_URL || 'https://api.mention.earth';
+export const SOCKET_URL = process.env.API_URL_SOCKET || 'https://api.mention.earth';
 
-export const API_URL_OXY = process.env.API_URL_OXY || "https://api.mention.earth";
-export const API_URL_SOCKET = process.env.API_URL_SOCKET || "ws://api.mention.earth"; // Remove  suffix
-export const OXY_CLOUD_URL = process.env.OXY_CLOUD_URL || "https://api.mention.earth/files/";
+export const API_URL_SOCKET = process.env.API_URL_SOCKET || "ws://api.mention.earth";
 export const API_URL_SOCKET_CHAT = process.env.API_URL_SOCKET_CHAT || "https://api.mention.earth";
 export const API_OXY_CHAT = process.env.API_OXY_CHAT || "https://api.mention.earth";
