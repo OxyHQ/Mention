75ce510a107746a88f517fe4a161c1ed70468e50
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2025-03-02 18:25:17 +0100

    Fix stroke-width property to strokeWidth in AnalyticsIcon components

--- assets/icons/analytics-icon.tsx
+++ assets/icons/analytics-icon.tsx
@@ -6,7 +6,7 @@ import { colors } from '@/styles/colors';
 export const AnalyticsIcon = ({ color = colors.primaryColor, size = 26, style }: { color?: string; size?: number; style?: ViewStyle }) => {
   return (
     <Svg viewBox="0 0 24 24" width={size} height={size} style={{ ...style }}>
-      <Rect fill="none" height="20" rx="5" stroke={color} stroke-width="2" width="20" x="2" y="2"></Rect>
+      <Rect fill="none" height="20" rx="5" stroke={color} strokeWidth="2" width="20" x="2" y="2"></Rect>
       <Rect height="12" rx="1" width="2" x="11" y="6" fill={color}></Rect>
       <Rect height="9" rx="1" width="2" x="15" y="9" fill={color}></Rect>
       <Rect height="5" rx="1" width="2" x="7" y="13" fill={color}></Rect>
@@ -17,7 +17,7 @@ export const AnalyticsIcon = ({ color = colors.primaryColor, size = 26, style }:
 export const AnalyticsIconActive = ({ color = colors.primaryColor, size = 26, style }: { color?: string; size?: number; style?: ViewStyle }) => {
   return (
     <Svg viewBox="0 0 24 24" width={size} height={size} style={{ ...style }}>
-<Rect fill="none" height="20" rx="5" stroke={color} stroke-width="2" width="20" x="2" y="2"></Rect>
+<Rect fill="none" height="20" rx="5" stroke={color} strokeWidth="2" width="20" x="2" y="2"></Rect>
       <Rect height="12" rx="1" width="2.5" x="11" y="6" fill={color}></Rect>
       <Rect height="9" rx="1" width="2.5" x="15" y="9" fill={color}></Rect>
       <Rect height="5" rx="1" width="2.5" x="7" y="13" fill={color}></Rect>
