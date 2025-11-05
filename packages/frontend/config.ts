1144f69c920d420aa7d13438d01c8579af426676
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2025-11-05 11:35:16 +0100

    chore: Update API_URL in config.ts to include '/api' in the default localhost path for improved local development consistency

--- packages/frontend/config.ts
+++ packages/frontend/config.ts
@@ -3,7 +3,7 @@
 export const API_URL =
   process.env.NODE_ENV === 'production'
     ? 'https://api.mention.earth/api/'
-    : (process.env.API_URL ?? 'http://localhost:3000');
+    : (process.env.API_URL ?? 'http://localhost:3000/api');
 export const SOCKET_URL = process.env.API_URL_SOCKET || 'ws://localhost:3000/';
 export const API_URL_SOCKET = process.env.API_URL_SOCKET || "ws://localhost:3000";
 
