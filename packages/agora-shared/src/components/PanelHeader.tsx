82360944ca1fa8f19898debef770a78f7901632c
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2026-02-09 21:54:53 +0100

    feat: adjust horizontal padding in PanelHeader component

--- packages/agora-shared/src/components/PanelHeader.tsx
+++ packages/agora-shared/src/components/PanelHeader.tsx
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
-    paddingHorizontal: 16,
+    paddingHorizontal: 8,
     paddingVertical: 12,
     borderBottomWidth: 1,
   },
