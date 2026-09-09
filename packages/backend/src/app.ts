abdf64f19df6939778e8d950b9d1a1531742d3f7
Nate Isern <nate@oxy.so>
2026-09-10 01:34:06 +0300

    fix(cors): allow anonymous edge region header

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -92,7 +92,7 @@ export function createApp(deps: CreateAppDependencies): express.Express {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     res.setHeader(
       'Access-Control-Allow-Headers',
-      'Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version',
+      'Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version, X-Oxy-Edge-Region',
     );
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     if (
